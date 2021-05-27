441dd646e43a35e470acb66bf416327a007459ca
Andres ricardo melo castellanos <fake260295@outlook.com>
2021-05-26 20:24:02 -0500

    cambio ip

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
+const hostname = '127.0.0.2';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola Mundo\n');
+  res.end('Hola Mundo\n Nuevo');
 });
 
 server.listen(port, hostname, () => {
